33e5a9efeece52d4bbb300ef9e332d862b928053
Tim Ross <tim@timross.info>
2011-04-07 11:00:49 +1200

    Updated license info

--- SimpleShare/Classes/Example/RootViewController.m
+++ SimpleShare/Classes/Example/RootViewController.m
@@ -3,7 +3,6 @@
 //  SimpleShare
 //
 //  Created by Timothy Ross on 06/04/2011.
-//  Copyright 2011 Blue Key Digital Limited. All rights reserved.
 //
 
 #import "RootViewController.h"
--- SimpleShare/Classes/SimpleShare/NSString+URLEscape.m
+++ SimpleShare/Classes/SimpleShare/NSString+URLEscape.m
@@ -1,9 +1,8 @@
 //
-//  URLHelper.m
+//  NSString+URLEscape.m
 //  SimpleShare
 //
 //  Created by Timothy Ross on 06/04/2011.
-//  Copyright 2011 Blue Key Digital Limited. All rights reserved.
 //
 
 #import "NSString+URLEscape.h"
